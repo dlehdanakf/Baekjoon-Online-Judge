e5d3a5458786efd6a7813c33ce294ca0d96d1057
LEE Dong Hyun <dlehdanakf@konkuk.ac.kr>
2016-07-13 17:47:01 +0900

    HelloWorld 문제

---
+++ Baekjoon-Online-Judge/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ *	HelloWorld
+ *	https://www.acmicpc.net/problem/2557
+ *	2016-07-13
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
