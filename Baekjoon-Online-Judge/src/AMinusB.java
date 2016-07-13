6f5e23d6ba17a46042edefa28eea3482ee91b047
LEE Dong Hyun <dlehdanakf@konkuk.ac.kr>
2016-07-13 18:00:17 +0900

    A-B 문제

---
+++ Baekjoon-Online-Judge/src/AMinusB.java
@@ -0,0 +1,15 @@
+/**
+ *	A - B
+ *	https://www.acmicpc.net/problem/1000
+ *	2016-07-13
+ */
+import java.util.Scanner;
+public class AMinusB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a - b);
+	}
+}
